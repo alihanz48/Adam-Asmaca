2ed6dc8a225f1c08c53f666595d1814b8318e4d1
alihanz48 <dursunalihan@gmail.com>
2023-09-07 21:53:21 +0300

    Adam Asmaca
    C# ile yapılmış adam asmaca oyunu.

---
+++ Adam Asmaca/Adam Asmaca/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿
+namespace Adam_Asmaca
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.p1 = new System.Windows.Forms.Panel();
+            this.p2 = new System.Windows.Forms.Panel();
+            this.p3 = new System.Windows.Forms.Panel();
+            this.p4 = new System.Windows.Forms.Panel();
+            this.p5 = new System.Windows.Forms.PictureBox();
+            this.p6 = new System.Windows.Forms.PictureBox();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.p5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p6)).BeginInit();
+            this.panel9.SuspendLayout();
+            this.panel10.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Yellow;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(230, 93);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(72, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Başla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(72, 30);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Harf sayısı : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(8, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Harf gir : ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 146);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(112, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(195, 144);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Tamam";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Strikeout, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(293, 169);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(180, 204);
+            this.listBox1.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel3.Location = new System.Drawing.Point(274, 144);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(9, 237);
+            this.panel3.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(289, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Hatalı harfler";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Location = new System.Drawing.Point(15, 62);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(586, 60);
+            this.panel2.TabIndex = 8;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(64, 2);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(214, 54);
+            this.panel4.TabIndex = 0;
+            // 
+            // p1
+            // 
+            this.p1.BackColor = System.Drawing.Color.Black;
+            this.p1.Location = new System.Drawing.Point(59, 391);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(136, 10);
+            this.p1.TabIndex = 9;
+            this.p1.Visible = false;
+            // 
+            // p2
+            // 
+            this.p2.BackColor = System.Drawing.Color.Black;
+            this.p2.Location = new System.Drawing.Point(195, 391);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(136, 10);
+            this.p2.TabIndex = 10;
+            this.p2.Visible = false;
+            // 
+            // p3
+            // 
+            this.p3.BackColor = System.Drawing.Color.Black;
+            this.p3.Location = new System.Drawing.Point(191, 47);
+            this.p3.Name = "p3";
+            this.p3.Size = new System.Drawing.Size(10, 343);
+            this.p3.TabIndex = 11;
+            this.p3.Visible = false;
+            // 
+            // p4
+            // 
+            this.p4.BackColor = System.Drawing.Color.Black;
+            this.p4.Location = new System.Drawing.Point(65, 36);
+            this.p4.Name = "p4";
+            this.p4.Size = new System.Drawing.Size(136, 10);
+            this.p4.TabIndex = 10;
+            this.p4.Visible = false;
+            // 
+            // p5
+            // 
+            this.p5.BackColor = System.Drawing.Color.Transparent;
+            this.p5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("p5.BackgroundImage")));
+            this.p5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.p5.Location = new System.Drawing.Point(65, 46);
+            this.p5.Name = "p5";
+            this.p5.Size = new System.Drawing.Size(126, 124);
+            this.p5.TabIndex = 12;
+            this.p5.TabStop = false;
+            this.p5.Visible = false;
+            // 
+            // p6
+            // 
+            this.p6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("p6.BackgroundImage")));
+            this.p6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p6.Location = new System.Drawing.Point(65, 170);
+            this.p6.Name = "p6";
+            this.p6.Size = new System.Drawing.Size(126, 183);
+            this.p6.TabIndex = 13;
+            this.p6.TabStop = false;
+            this.p6.Visible = false;
+            // 
+            // panel9
+            // 
+            this.panel9.Controls.Add(this.panel2);
+            this.panel9.Controls.Add(this.label2);
+            this.panel9.Controls.Add(this.textBox1);
+            this.panel9.Controls.Add(this.button2);
+            this.panel9.Controls.Add(this.listBox1);
+            this.panel9.Controls.Add(this.panel3);
+            this.panel9.Controls.Add(this.label3);
+            this.panel9.Location = new System.Drawing.Point(12, 117);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(638, 392);
+            this.panel9.TabIndex = 14;
+            // 
+            // panel10
+            // 
+            this.panel10.Controls.Add(this.p6);
+            this.panel10.Controls.Add(this.p2);
+            this.panel10.Controls.Add(this.p4);
+            this.panel10.Controls.Add(this.p3);
+            this.panel10.Controls.Add(this.p1);
+            this.panel10.Controls.Add(this.p5);
+            this.panel10.Location = new System.Drawing.Point(797, 68);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(352, 438);
+            this.panel10.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1161, 521);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel10);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.p5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p6)).EndInit();
+            this.panel9.ResumeLayout(false);
+            this.panel9.PerformLayout();
+            this.panel10.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel p1;
+        private System.Windows.Forms.Panel p2;
+        private System.Windows.Forms.Panel p3;
+        private System.Windows.Forms.Panel p4;
+        private System.Windows.Forms.PictureBox p5;
+        private System.Windows.Forms.PictureBox p6;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel10;
+    }
+}
+
---
+++ Adam Asmaca/Adam Asmaca/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adam_Asmaca
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string skelime;
+        string[,] kelimeler = new string[8, 3] { { "ASA", "TEL", "YER" }, { "KOMİ", "ARKA", "KARE" }, { "DENİZ", "GÖLET", "YOKUŞ" }, { "ŞALTER", "TERLİK", "HATIRA" }, { "TELEFON", "OYUNCAK", "ASKILIK" }, { "KARAKTER", "ŞARAPNEL", "PITIRCIK" }, { "BABAYİĞİT", "FAGOSİTOZ", "JEOTERMAL" }, { "TERBİYESİZ", "ABDESTHANE", "RADYOAKTİF" } };
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel9.Visible = false;
+            p5.Location = new Point((p4.Location.X-(p5.Width/2)),p4.Location.Y+p4.Height);
+            p6.Location = new Point(p5.Location.X,((p6.Location.Y-(p5.Height/3))));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Enabled = false;
+            panel9.Visible = true;  
+            Random rndm = new Random();
+            skelime = kelimeler[(Convert.ToInt32(numericUpDown1.Value) - 3), rndm.Next(0, 3)];
+            int harfuzunluk = 1;
+            int konum = 0;
+
+            for (int i = 0; i < skelime.Length; i++)
+            {
+                Label harf = new Label();
+                harf.Font = new Font(harf.Font.FontFamily,36,harf.Font.Style);
+                harf.Location = new Point(konum,0);
+                harf.Height = 55;
+                harf.Width = 50;
+                konum += 50;
+                harfuzunluk = 50;
+                harf.Text = skelime[i].ToString();
+                panel4.Controls.Add(harf);
+                harf.Visible = false;
+            }
+            panel4.Width = 50 * skelime.Length;
+            panel4.Location = new Point((panel2.Width-panel4.Width)/2,(panel2.Height-panel4.Height)/2);
+        }
+
+        bool hatadrm = true;
+        bool mevcutharf = false;
+        int hatasay = 0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            hatadrm = true;
+            mevcutharf = false;
+            foreach (Control harf in panel4.Controls)
+            {
+                if (harf.Text == textBox1.Text.ToUpper())
+                {
+                    if (harf.Visible==true)
+                    {
+                        mevcutharf = true;
+                    }
+                    harf.Visible = true;
+                    hatadrm = false;
+                }  
+            }
+
+            if (mevcutharf==true)
+            {
+                MessageBox.Show("Bu harf zaten açılmış durumda.");
+            }
+
+            if (hatadrm ==true)
+            {
+                hatasay++;
+                foreach (Control parca in panel10.Controls)
+                {
+                    if (parca.Name == "p" + hatasay)
+                    {
+                        parca.Visible = true;
+                    }
+                }
+
+                if (p6.Visible == true)
+                {
+                    MessageBox.Show("Maalesef,Kaybettiniz !");
+                    Application.Restart();
+                }
+            }
+
+            int aharf = 0;
+            foreach (Control harfsay in panel4.Controls)
+            {
+                if (harfsay.Visible==true)
+                {
+                    aharf++;
+                }
+
+                if (skelime.Length==aharf)
+                {
+                    MessageBox.Show("Tebrikler,Kazandınız !");
+                    Application.Restart();
+                }
+            }
+
+            if (!skelime.Contains(textBox1.Text.ToUpper())&&!listBox1.Items.Contains(textBox1.Text.ToUpper()))
+            {
+                listBox1.Items.Add(textBox1.Text);
+            }
+            textBox1.Text = null;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length>0)
+            {
+                string harf = textBox1.Text;
+                textBox1.Text = harf.Substring(0, 1).ToUpper();
+            }
+        }
+    }
+}
